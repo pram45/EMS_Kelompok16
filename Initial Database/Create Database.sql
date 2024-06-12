1bf525c7e7710b6887189daf0962505fd47cd489
pram45 <darilgh13@gmail.com>
2024-06-12 21:09:55 +0700

    Create Create Database.sql
    membuat database untuk employee management system

---
+++ Initial Database/Create Database.sql
@@ -0,0 +1,11 @@
+-- ===========================================
+-- Author:	pram45
+-- Create date: 11-06-2024
+-- Description:	<Membuat databsae>
+-- =============================================
+
+CREATE DATABASE db_EMS
+GO
+
+USE db_EMS
+GO
