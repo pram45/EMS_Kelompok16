06d52a95c827b1cee15dff290ad5bf649303f779
pram45 <darilgh13@gmail.com>
2024-06-15 23:01:04 +0700

    passwordpolicy

---
+++ function/func_checkpasswordpolicy.sql
@@ -0,0 +1,32 @@
+CREATE FUNCTION fn_CheckPasswordPolicy(@AccountID INT)
+RETURNS NVARCHAR(50)
+AS
+BEGIN
+    DECLARE @Result NVARCHAR(100);
+    DECLARE @Password NVARCHAR(128);
+
+    SELECT @Password = A.password FROM tbl_accounts A WHERE A.id = @AccountID;
+
+    IF LEN(@Password) < 8
+    BEGIN
+        SET @Result = 'Password harus memiliki minimal 8 karakter.';
+    END
+    ELSE IF NOT @Password LIKE '%[0-9]%'
+    BEGIN
+        SET @Result = 'Password harus memiliki setidaknya 1 angka.';
+    END
+	ELSE IF NOT @Password LIKE '%[A-Z]%'
+	BEGIN 
+		SET @Result = 'Password harus memiliki setidaknya 1 huruf besar.';
+	END
+    ELSE IF NOT @Password LIKE '%[a-z]%'
+    BEGIN
+        SET @Result = 'Password harus memiliki setidaknya 1 huruf.';
+    END
+    ELSE
+    BEGIN
+        SET @Result = 'Password memenuhi kebijakan.';
+    END
+
+    RETURN @Result;
+END;
