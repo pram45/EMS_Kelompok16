a59c794a1c8ba72ff4b9c7659108683c0192b6d3
Rafi446 <rfdl11611@gmail.com>
2024-06-16 00:08:41 +0700

    Create add constraint FK table.sql

---
+++ Initial Table/add constraint FK table.sql
@@ -0,0 +1,24 @@
+-- FK_id_from_tbl_employees
+ALTER TABLE tbl_accounts 
+ADD CONSTRAINT FK_id_from_tbl_employees
+FOREIGN KEY (id) REFERENCES tbl_employees (id);
+
+-- FK_account_from_tbl_accounts
+ALTER TABLE tbl_account_roles
+ADD CONSTRAINT FK_account_from_tbl_accounts
+FOREIGN KEY (account) REFERENCES tbl_accounts (id);
+
+-- FK_role_from_tbl_roles
+ALTER TABLE tbl_account_roles
+ADD CONSTRAINT FK_role_from_tbl_roles
+FOREIGN KEY (role) REFERENCES tbl_roles (id);
+
+-- FK_role_from_tbl_roles
+ALTER TABLE tbl_role_permissions
+ADD CONSTRAINT FK_role_from_tbl_roles_id
+FOREIGN KEY (role) REFERENCES tbl_roles (id);
+
+-- FK_permissions_from_tbl_permissions
+ALTER TABLE tbl_role_permissions
+ADD CONSTRAINT FK_permission_from_tbl_permissions_id
+FOREIGN KEY (role) REFERENCES tbl_permissions (id);
