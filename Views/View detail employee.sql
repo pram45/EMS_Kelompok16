cb0186ffce58c1061795def5e1be7754271f3b24
Rafi446 <rfdl11611@gmail.com>
2024-06-18 09:11:17 +0700

    Update View detail employee.sql

--- Views/View detail employee.sql
+++ Views/View detail employee.sql
@@ -1,21 +1,43 @@
 CREATE VIEW vw_DetailEmployee
 AS
 SELECT 
-    tbl_employees.id, 
-    tbl_employees.first_name, 
-    tbl_employees.last_name, 
-    tbl_employees.gender, 
-    tbl_employees.email, 
-    tbl_employees.phone, 
-    tbl_employees.hire_date, 
-    tbl_employees.salary, 
-    tbl_employees.manager, 
-    tbl_employees.job, 
-    tbl_employees.department
+    e.id, 
+    e.first_name + ' ' + e.last_name AS full_name, 
+    e.gender, 
+    e.email, 
+    e.phone, 
+    e.hire_date, 
+    e.salary, 
+    e.manager, 
+    e.job, 
+    e.department, 
+    r.name AS name_role, 
+    l.street_address, 
+    l.postal_code, 
+    l.city, 
+    jh.status
 FROM 
-    tbl_departments 
+    tbl_account_roles ar
 INNER JOIN 
-    tbl_employees ON tbl_departments.id = tbl_employees.department 
+    tbl_accounts a ON ar.account = a.id
 INNER JOIN 
-    tbl_jobs ON tbl_employees.job = tbl_jobs.id;
+    tbl_departments d ON ar.id = d.id
+INNER JOIN 
+    tbl_employees e ON a.id = e.id
+INNER JOIN 
+    tbl_job_histories jh ON d.id = jh.department AND e.id = jh.employee
+INNER JOIN 
+    tbl_jobs j ON jh.job = j.id
+INNER JOIN 
+    tbl_locations l ON d.location = l.id
+INNER JOIN 
+    tbl_countries c ON l.country = c.id
+INNER JOIN 
+    tbl_permissions p ON ar.id = p.id
+INNER JOIN 
+    tbl_regions rg ON c.region = rg.id
+INNER JOIN 
+    tbl_role_permissions rp ON p.id = rp.permission
+INNER JOIN 
+    tbl_roles r ON ar.role = r.id AND rp.role = r.id;
 GO
