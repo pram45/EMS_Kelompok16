33b7398804b3acde4aba7701e1890f762ff3d453
Rafi446 <rfdl11611@gmail.com>
2024-06-18 09:22:07 +0700

    Update View detail employee.sql

--- Views/View detail employee.sql
+++ Views/View detail employee.sql
@@ -1,43 +1,43 @@
 CREATE VIEW vw_DetailEmployee
 AS
-SELECT 
-    e.id, 
-    e.first_name + ' ' + e.last_name AS full_name, 
-    e.gender, 
-    e.email, 
-    e.phone, 
-    e.hire_date, 
-    e.salary, 
-    e.manager, 
-    e.job, 
-    e.department, 
-    r.name AS name_role, 
-    l.street_address, 
-    l.postal_code, 
-    l.city, 
-    jh.status
-FROM 
+SELECT
+    e.id,
+    e.first_name + ' ' + e.last_name AS full_name,
+    e.gender,
+    e.email,
+    e.phone,
+    e.hire_date,
+    e.salary,
+    e.manager,
+    e.department,
+    r.name AS name_role,
+    l.street_address,
+    l.postal_code,
+    l.city,
+    jh.status,
+    j.title AS job_name
+FROM
     tbl_account_roles ar
-INNER JOIN 
+INNER JOIN
     tbl_accounts a ON ar.account = a.id
-INNER JOIN 
+INNER JOIN
     tbl_departments d ON ar.id = d.id
-INNER JOIN 
+INNER JOIN
     tbl_employees e ON a.id = e.id
-INNER JOIN 
-    tbl_job_histories jh ON d.id = jh.department AND e.id = jh.employee
-INNER JOIN 
+INNER JOIN
+    tbl_job_histories jh ON d.id = jh.department AND e.id = jh.employee AND e.id = jh.employee
+INNER JOIN
     tbl_jobs j ON jh.job = j.id
-INNER JOIN 
+INNER JOIN
     tbl_locations l ON d.location = l.id
-INNER JOIN 
+INNER JOIN
     tbl_countries c ON l.country = c.id
-INNER JOIN 
+INNER JOIN
     tbl_permissions p ON ar.id = p.id
-INNER JOIN 
+INNER JOIN
     tbl_regions rg ON c.region = rg.id
-INNER JOIN 
+INNER JOIN
     tbl_role_permissions rp ON p.id = rp.permission
-INNER JOIN 
+INNER JOIN
     tbl_roles r ON ar.role = r.id AND rp.role = r.id;
 GO
