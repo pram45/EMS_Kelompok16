c622ec1b82cffcae680308b0609adbdd5ccda1fb
Rafi446 <rfdl11611@gmail.com>
2024-06-18 09:26:05 +0700

    Update View detail employee.sql

--- Views/View detail employee.sql
+++ Views/View detail employee.sql
@@ -1,6 +1,6 @@
-CREATE VIEW vw_DetailEmployee
+CREATE VIEW dbo.vw_EmployeeDetails
 AS
-SELECT
+SELECT 
     e.id,
     e.first_name + ' ' + e.last_name AS full_name,
     e.gender,
@@ -9,35 +9,35 @@ SELECT
     e.hire_date,
     e.salary,
     e.manager,
-    e.department,
     r.name AS name_role,
     l.street_address,
     l.postal_code,
     l.city,
     jh.status,
-    j.title AS job_name
-FROM
-    tbl_account_roles ar
-INNER JOIN
-    tbl_accounts a ON ar.account = a.id
-INNER JOIN
-    tbl_departments d ON ar.id = d.id
-INNER JOIN
-    tbl_employees e ON a.id = e.id
-INNER JOIN
-    tbl_job_histories jh ON d.id = jh.department AND e.id = jh.employee AND e.id = jh.employee
-INNER JOIN
-    tbl_jobs j ON jh.job = j.id
-INNER JOIN
-    tbl_locations l ON d.location = l.id
-INNER JOIN
-    tbl_countries c ON l.country = c.id
-INNER JOIN
-    tbl_permissions p ON ar.id = p.id
-INNER JOIN
-    tbl_regions rg ON c.region = rg.id
-INNER JOIN
-    tbl_role_permissions rp ON p.id = rp.permission
-INNER JOIN
-    tbl_roles r ON ar.role = r.id AND rp.role = r.id;
+    j.title AS job_name,
+    d.name AS department_name
+FROM 
+    dbo.tbl_account_roles ar
+INNER JOIN 
+    dbo.tbl_accounts a ON ar.account = a.id
+INNER JOIN 
+    dbo.tbl_departments d ON ar.id = d.id
+INNER JOIN 
+    dbo.tbl_employees e ON a.id = e.id
+INNER JOIN 
+    dbo.tbl_job_histories jh ON d.id = jh.department AND e.id = jh.employee
+INNER JOIN 
+    dbo.tbl_jobs j ON jh.job = j.id
+INNER JOIN 
+    dbo.tbl_locations l ON d.location = l.id
+INNER JOIN 
+    dbo.tbl_countries c ON l.country = c.id
+INNER JOIN 
+    dbo.tbl_permissions p ON ar.id = p.id
+INNER JOIN 
+    dbo.tbl_regions rg ON c.region = rg.id
+INNER JOIN 
+    dbo.tbl_role_permissions rp ON p.id = rp.permission
+INNER JOIN 
+    dbo.tbl_roles r ON ar.role = r.id AND rp.role = r.id;
 GO
